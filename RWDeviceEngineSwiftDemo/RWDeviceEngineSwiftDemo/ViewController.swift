e1f8853bcbeb3996068f402d743073622be2fb1d
Rover001 <zengyun6666@163.com>
2019-11-28 13:19:28 +0800

    0.2.1版本。使用carthage打静态库 测试是否可以使用

---
+++ RWDeviceEngineSwiftDemo/RWDeviceEngineSwiftDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RWDeviceEngineSwiftDemo
+//
+//  Created by linmao on 2019/11/28.
+//  Copyright © 2019 RoverWord. All rights reserved.
+//
+
+import UIKit
+import DeviceEngineSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
